52d432b5235566b6b3fd7610d674ac6e6b5b8e95
mitchell_west <mitchell.west@7-11.com>
2019-12-06 08:53:00 -0600

    Updated logger service/model

--- src/app/models/logger.model.ts
+++ src/app/models/logger.model.ts
@@ -2,22 +2,28 @@ import winston from 'winston';
 import { ColorizeOptions } from 'logform';
 import util from 'util';
 
-
 /* This class creates a winston logger and transports when instantiated with the necessary configuration options. A console logger transport is created by default
     and other transports can be added if the configurations are included */
 export default class WinstonLogger {
-    private transports: Array<winston.transports.ConsoleTransportInstance | winston.transports.FileTransportInstance | winston.transports.HttpTransportInstance |
-        winston.transports.StreamTransportInstance> = [];
     private logger: winston.Logger;
 
-    constructor (minimumLevel: string, showLogs: boolean, levels: winston.LoggerOptions['levels'], colors?: ColorizeOptions['colors'], namespace?: string, consoleOptions?: winston.transports.ConsoleTransportOptions,
-        fileOptions?: winston.transports.FileTransportOptions, httpOptions?: winston.transports.HttpTransportOptions, streamOptions?: winston.transports.StreamTransportOptions) {
-
-        /* Create the logger transports */
-        this.transports.push(new winston.transports.Console(consoleOptions || undefined));
-        if (fileOptions) this.transports.push(new winston.transports.File(fileOptions));
-        if (httpOptions) this.transports.push(new winston.transports.Http(httpOptions));
-        if (streamOptions) this.transports.push(new winston.transports.Stream(streamOptions));
+    constructor(minimumLevel: string, levels: winston.LoggerOptions['levels'], colors?: ColorizeOptions['colors'], namespace?: string, logConsole?: boolean, logFilePath?: string) {
+        const transports = [];
+        if (logConsole) transports.push(new winston.transports.Console());
+        if (logFilePath) {
+            const fileName = `log_${new Date()
+                .toISOString()
+                .replace(/-/g, '_')
+                .replace(/:/g, '_')
+                .replace(/\./g, '_')}`;
+            transports.push(
+                new winston.transports.File({
+                    filename: fileName,
+                    dirname: logFilePath,
+                    level: minimumLevel
+                })
+            );
+        }
 
         /* create the logger */
         this.logger = winston.createLogger({
@@ -27,20 +33,23 @@ export default class WinstonLogger {
                 winston.format.prettyPrint(),
                 winston.format.timestamp(),
                 winston.format.splat(),
-                colors ? winston.format.colorize({
-                    level: true, colors: colors
-                }) : undefined,
+                colors
+                    ? winston.format.colorize({
+                          level: true,
+                          colors: colors
+                      })
+                    : undefined,
                 winston.format.printf(msg => `${msg.timestamp} ${namespace ? `[${namespace}]` : null} ${msg.level}: ${msg.message}`)
             ),
-            transports: this.transports,
+            transports: transports,
             exitOnError: false,
-            silent: !showLogs
+            silent: !logConsole
         });
     }
 
     /* This adds a log method that allows for both strings or objects to be passed and prevents any issues with JSON.stringify on objects with a circular reference */
     log = (message: string | object, level: string) => {
-        if (typeof message === 'string') this.logger.log(level, message);
+        if (typeof message !== 'object') this.logger.log(level, message);
         if (typeof message === 'object') this.logger.log(level, util.format('%o', message));
-    }
+    };
 }
--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -1,14 +1,29 @@
 import WinstonLogger from '@model/logger.model';
+import fs from 'fs';
 
 /* Instantiates a winston logger and dynamically creates methods for each log level defined in enums.
     It allows for a logger[level] method instead of directly using WinstonLogger.log() */
 const logger: any = {
     init: (levels: any, colors: any) => {
         if (!levels) throw new Error('Log levels must be defined');
-        let winstonInstance = new WinstonLogger(process.env.DEFAULTLOGLEVEL || 'info', process.env.LOGCONSOLE.toUpperCase() === 'FALSE' ? false : true,
-            levels, colors, process.env.NAMESPACE || undefined);
-        Object.keys(levels).forEach(level => logger[level] = (message: string | object) => winstonInstance.log(message, level));
+        // if (!fs.existsSync('./logs')) fs.mkdirSync('./logs');
+        /* Removes logs over 30 count in folder */
+        // fs.readdirSync('./logs')
+        //     .sort()
+        //     .reverse()
+        //     .filter((logName, i) => i >= 29)
+        //     .map(logName => fs.unlinkSync(`./logs/${logName}`));
+        /* Create winston logger */
+        let winstonInstance = new WinstonLogger(
+            process.env.DEFAULTLOGLEVEL || 'info',
+            levels,
+            colors,
+            process.env.NAMESPACE || undefined,
+            process.env.LOGCONSOLE.toUpperCase() === 'FALSE' ? false : true /*,
+            './logs' */
+        );
+        Object.keys(levels).forEach(level => (logger[level] = (message: string | object) => winstonInstance.log(message, level)));
     }
-}
+};
 
 export default logger;
